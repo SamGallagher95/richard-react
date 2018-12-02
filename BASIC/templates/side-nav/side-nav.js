a05fbcaf3cca3bcfe59bfa2803de1c45c88654e1
Sam Gallagher <sam@77creative.io>
2018-12-02 10:14:04 -0600

    added sidebar template to all pages

--- BASIC/templates/side-nav/side-nav.js
+++ BASIC/templates/side-nav/side-nav.js
@@ -1,6 +1,18 @@
 function main() {
+  // Set the active link
+  const activePage = window.location.pathname;
+
+  // Get all of the links
+  const links = document.querySelectorAll("#sidebar a");
+  links.forEach(link => {
+    console.log(link.getAttribute("href"));
+    if (activePage.includes(link.getAttribute("href"))) {
+      link.classList.add("active");
+    }
+  });
+
+  // Show the sidebar
   document.getElementById("sidebar").classList.add("ready");
-  console.log("hell0");
 }
 
 setTimeout(() => {
